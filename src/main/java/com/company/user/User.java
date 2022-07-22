38ea8a91831434219e3c5196690ab9c8b39623a4
Josué Ávalos <aezequiel56@gmail.com>
2022-07-22 11:14:36 -0600

    CRUD FOR USER WITH HYBERNATE AND JPA

---
+++ src/main/java/com/company/user/User.java
@@ -0,0 +1,31 @@
+package com.company.user;
+
+import javax.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+
+@Entity
+@Table(name = "users")
+public class User {
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  @Getter @Setter
+  private Integer id;
+
+  @Column(nullable = false, unique = true, length = 45)
+  @Getter @Setter
+  private String email;
+
+  @Column(length = 15, nullable = false)
+  @Getter @Setter
+  private String password;
+
+  @Column(length = 15, nullable = false, name = "first_name")
+  @Getter @Setter
+  private String firstName;
+
+  @Column(length = 15, nullable = false, name = "last_name")
+  @Getter @Setter
+  private String lastName;
+}
---
+++ src/main/java/com/company/user/UserRepository.java
@@ -0,0 +1,7 @@
+package com.company.user;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface UserRepository extends CrudRepository<User, Integer> {
+
+}
